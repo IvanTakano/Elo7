0aa5fc9f2aa3d5f175326da315c7b7035deb2ef0
ivanyuratakano <ivanyuratakano@192.168.0.9>
2020-12-04 07:18:13 -0300

    04/10

---
+++ src/elo7/Mapa.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elo7;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ivanyuratakano
+ */
+public class Mapa {
+    private String[][] ambiente;
+    
+    public Mapa(){
+        ambiente = matriz();
+    }
+    
+    public static String [][] matriz (){
+        
+        Scanner entrada = new Scanner(System.in);
+        
+       
+        System.out.println("Digite as coordenadas do ponto superior-direito de Marte: ");
+        String trans = entrada.nextLine();
+        String[] coordenadas = trans.split(" ");        
+        int xConv = Integer.parseInt(coordenadas[0]);        
+        int yConv = Integer.parseInt(coordenadas[1]);                 
+        String[][] matriz = new String[xConv+1][yConv+1];
+        return matriz;
+    }
+
+    /**
+     * @return the ambiente
+     */
+    public String[][] getAmbiente() {
+        return ambiente;
+    }
+
+    /**
+     * @param ambiente the ambiente to set
+     */
+    public void setAmbiente(String[][] ambiente) {
+        this.ambiente = ambiente;
+    }
+    
+}
